d449d431a7a311f5e552c03675044b973548f54d
tamashiro-syuta <syuta5825@gmail.com>
2022-12-18 15:17:09 +0900

    ✅ active_jobのテストを追加

--- test/jobs/async_log_job_test.rb
+++ test/jobs/async_log_job_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AsyncLogJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Enqueue AsyncLogJob" do
+    # assert_enqueued_with アサーションを使ってキューにジョブが追加されているかどうかテスト
+    assert_enqueued_with(job: AsyncLogJob) do
+      AsyncLogJob.perform_later(message: "form test")
+    end
+  end
 end
