7e708ffedc1d859ef073d4ee018db41abbb37031
tamashiro-syuta <syuta5825@gmail.com>
2022-12-18 14:45:24 +0900

    👔 引数のメッセージで分岐して、キューを選別

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,6 +1,16 @@
 class AsyncLogJob < ApplicationJob
   # async_logというキューにジョブを追加するように設定
-  queue_as :async_log
+  # queue_as :async_log
+
+  # 引数のメッセージで分岐して、キューを選別
+  queue_as do
+    case self.arguments.first[:message]
+    when "to async_log"
+      :async_log
+    else
+      :default
+    end
+  end
 
   # 非同期処理時に呼ばれる
   def perform(message: 'hello')
